2507a1bc7c77b3f380e802198da703470a66d4ba
alexandrapowell8080-pixel <alexandrapowell8080@gmail.com>
2026-04-21 17:15:16 +0300

    feat:library pages

--- routes/web.php
+++ routes/web.php
@@ -12,8 +12,29 @@ Route::get('/course/{exam_slug}', [ExamController::class, 'category'])
 
 Route::get('/quiz/{exam_slug}/{subject_slug}/{test_slug}', [ExamController::class, 'quiz'])
     ->where([
-        'exam_slug' => '[a-z0-9-]+', 
-        'subject_slug' => '[a-z0-9-]+', 
+        'exam_slug' => '[a-z0-9-]+',
+        'subject_slug' => '[a-z0-9-]+',
         'test_slug' => '[a-z0-9-]+'
     ])
     ->name('quiz.show');
+
+Route::get('/quiz/{exam_slug}/{subject_slug}/{test_slug}/results', [ExamController::class, 'results'])
+    ->where([
+        'exam_slug' => '[a-z0-9-]+',
+        'subject_slug' => '[a-z0-9-]+',
+        'test_slug' => '[a-z0-9-]+'
+    ])
+    ->name('quiz.results');
+
+Route::get('/health', function () {
+    return response()->json([
+        'status' => 'ok',
+        'timestamp' => now(),
+        'app' => config('app.name'),
+        'env' => config('app.env'),
+    ]);
+});
+
+Route::fallback(function () {
+    return response()->view('errors.404', [], 404);
+});
